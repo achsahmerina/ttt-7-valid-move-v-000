94cc8bd09b6dfb06d53a339d2cd1d0a6d3636c5e
Achsah Abraham <achsahmp@gmail.com>
2018-09-24 18:37:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,25 @@
-def valid_move?(number, board)
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
 
-end
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 
-def position_taken?(board, index)
-  if board[index] ==  " " || board[index] == "" || board[index] == nil
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
+
+
 end
