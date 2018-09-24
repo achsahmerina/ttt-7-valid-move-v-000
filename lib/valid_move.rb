56bb5bff4bfce22c259be1f1f6e2681d072a32d1
Achsah Abraham <achsahmp@gmail.com>
2018-09-24 18:32:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,5 @@
 def valid_move?(number, board)
-  if board[index] ==  " " || board[index] == "" || board[index] == nil
-    return true
-  else
-    return false
-  end
+
 end
 
 def position_taken?(board, index)
