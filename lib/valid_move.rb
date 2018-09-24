51c4a73203d945621bf1f163816cfd8cc58e0d92
Achsah Abraham <achsahmp@gmail.com>
2018-09-24 18:38:31 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def valid_move?(board, index)
   end
 
   def on_board?(number)
-    if number.between?(0, 8) == true
+    if number.between?(0, 9) == true
       return true
     else
       return false
